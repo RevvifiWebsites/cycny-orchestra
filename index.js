ea31b3ed2475eebf4df261486c1eae26d296dde2
Wilbur W <wilburwei.2008@gmail.com>
2024-07-27 22:15:04 -0400

    Add expandable information to biographies

---
+++ index.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+  $(".bioopen").click(function() {
+    const display = ($("#" + $(this).attr("caction")).css("display"));
+    if (display == "none") {
+      $(this).html("Hide Biography")
+      $("#" + $(this).attr("caction")).css('opacity', 0);
+      $("#" + $(this).attr("caction")).show();
+      $("#" + $(this).attr("caction")).animate({
+        opacity:1,
+      }, 500)
+
+    } else {
+      $("#" + $(this).attr("caction")).css("display", "none");
+      $(this).html("Show Biography")
+
+      // $("#" + $(this).attr("caction")).animate({
+      //   opacity: 0,
+      // }, 500, function() {
+      //   console.log('ea')
+      // })
+
+
+    }
+  })
+}
