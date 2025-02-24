06a0e3407c52170ff1e7480d2909f28e71a9bd80
Wilbur W <wilburwei.2008@gmail.com>
2025-02-23 19:43:08 -0500

    Fix faculty biography showing bug

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-window.onload = function() {
-  $(".bioopen").click(function() {
-    const display = ($("#" + $(this).attr("caction")).css("display"));
-    console.log("je")
-    if (display == "none") {
-      $(this).html("Hide Biography")
-      $("#" + $(this).attr("caction")).css('opacity', 0);
-      $("#" + $(this).attr("caction")).show();
-      $("#" + $(this).attr("caction")).animate({
-        opacity:1,
-      }, 500)
+// window.onload = function() {
+//   $(".bioopen").click(function() {
+//     const display = ($("#" + $(this).attr("caction")).css("display"));
+//     console.log("je")
+//     if (display == "none") {
+//       $(this).html("Hide Biography")
+//       $("#" + $(this).attr("caction")).css('opacity', 0);
+//       $("#" + $(this).attr("caction")).show();
+//       $("#" + $(this).attr("caction")).animate({
+//         opacity:1,
+//       }, 500)
 
-    } else {
-      $("#" + $(this).attr("caction")).css("display", "none");
-      $(this).html("Show Biography")
+//     } else {
+//       $("#" + $(this).attr("caction")).css("display", "none");
+//       $(this).html("Show Biography")
 
-      // $("#" + $(this).attr("caction")).animate({
-      //   opacity: 0,
-      // }, 500, function() {
-      //   console.log('ea')
-      // })
+//       // $("#" + $(this).attr("caction")).animate({
+//       //   opacity: 0,
+//       // }, 500, function() {
+//       //   console.log('ea')
+//       // })
 
 
-    }
-  })
-}
+//     }
+//   })
+// }
