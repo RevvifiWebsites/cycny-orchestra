d3dc51223af95e09fa65385d7d1d29aae0ff2dd1
Wilbur W <wilburwei.2008@gmail.com>
2025-03-04 23:45:11 -0500

    Format all the code + add script where missing

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-window.onload = function() {
-  $("#c").on("click", function() {
-    window.location.href ="/"
+window.onload = function () {
+  $("#c").on("click", function () {
+    window.location.href = "/"
   })
-  $(".navlink").on("click", function() {
+  $(".navlink").on("click", function () {
     const go = $(this).html()
     switch (go) {
       case "About Us":
-        window.location.href ="/about.html"
+        window.location.href = "/about.html"
         break;
       case "News":
-        window.location.href ="/news.html"
+        window.location.href = "/news.html"
         break;
       case "Members":
-        window.location.href ="/members.html"
+        window.location.href = "/members.html"
         break;
       case "Auditions":
-        window.location.href ="/auditions.html"
+        window.location.href = "/auditions.html"
         break;
       case "Patrons":
-        window.location.href ="/patrons.html"
+        window.location.href = "/patrons.html"
         break;
 
     }
-  
+
   })
 }
